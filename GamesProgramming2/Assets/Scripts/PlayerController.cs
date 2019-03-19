10870b1480fa9a61441447eb0becccdb1403449c
Jammie506 <37370214+Jammie506@users.noreply.github.com>
2019-03-19 13:42:34 +0000

    PlayerController
    PlayerController base using animation and root motion

--- GamesProgramming2/Assets/Scripts/PlayerController.cs
+++ GamesProgramming2/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
+    Animator animator;
+    float verticalInput;
+    float horizontalInput;
+
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+    void FixedUpdate()
+    {
+        animator.SetFloat("vAxis", verticalInput);
+        animator.SetFloat("hAxis", horizontalInput);
         
+        if (Input.GetKey(KeyCode.Z))
+        {
+            animator.SetBool("Run", true);
+            Debug.Log("Run");
+
+        }
+        else
+        {
+            animator.SetBool("Run", false);
+            Debug.Log("No Run");
+        }
     }
 }
