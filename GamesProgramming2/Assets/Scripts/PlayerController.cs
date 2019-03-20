215fdd8e5f46c03122d2f6108d97116f51d0327b
Jammie506 <37370214+Jammie506@users.noreply.github.com>
2019-03-20 12:06:12 +0000

    Added Jump

--- GamesProgramming2/Assets/Scripts/PlayerController.cs
+++ GamesProgramming2/Assets/Scripts/PlayerController.cs
@@ -7,23 +7,41 @@ public class PlayerController : MonoBehaviour
     Animator animator;
     float verticalInput;
     float horizontalInput;
+    float jumpInput;
+
+    Rigidbody rB;
 
     void Start()
     {
         animator = GetComponent<Animator>();
+
+        rB = GetComponent<Rigidbody>();
     }
     
     void Update()
     {
         verticalInput = Input.GetAxis("Vertical");
         horizontalInput = Input.GetAxis("Horizontal");
+        jumpInput = Input.GetAxis("Jump");
     }
     void FixedUpdate()
     {
         animator.SetFloat("vAxis", verticalInput);
         animator.SetFloat("hAxis", horizontalInput);
-        
-        if (Input.GetKey(KeyCode.Z))
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            animator.SetBool("Jump", true);
+            Debug.Log("Jump");
+
+        }
+        else
+        {
+            animator.SetBool("Jump", false);
+            Debug.Log("No Jump");
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
         {
             animator.SetBool("Run", true);
             Debug.Log("Run");
